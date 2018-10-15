974b592943bc7b29ed854a3a470eabfdef3a30f7
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-15 14:49:13 +0000

    Refactored code, no it exports the function in the right way

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,13 +1,12 @@
-function valitadeInput(input){
-	if(input.length != 2){
-		throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+module.exports = {
+	valitadeInput: function(input){
+		if(input.length != 2){
+			throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+		}
+		var validationMask = /([xXoO(\d)])([(\d)xXoO])/;
+		if( !validationMask.exec(input) ){		
+			throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+		}
+		return input.toLowerCase();
 	}
-	var validationMask = /([xXoO(\d)])([(\d)xXoO])/;
-	if( !validationMask.exec(input) ){
-		
-		throw input + " is not a valid input only acceptable input is on the form x1 or o1";
-	}
-	return input.toLowerCase();
-}
-
-module.exports = valitadeInput;
+};
--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -3,16 +3,16 @@ const gameLogic = require("./gameLogic.js");
 
 it("Should throw an error with Error code 'notValidInput is not a valid input only acceptable input is on the form x1 or o1'", () => {
 	function tryInvalid (){
-		gameLogic('notValidInput');
+		gameLogic.valitadeInput('notValidInput');
 	}
 	expect(tryInvalid).toThrowError("notValidInput is not a valid input only acceptable input is on the form x1 or o1");
 });
 it("Should throw an error with Error code 'bb is not a valid input only acceptable input is on the form x1 or o1'", () => {
 	function tryInvalid (){
-		gameLogic('bb');
+		gameLogic.valitadeInput('bb');
 	}
 	expect(tryInvalid).toThrowError("bb is not a valid input only acceptable input is on the form x1 or o1");
 });
 it("should return x1", () => {
-	expect(gameLogic("X1")).toBe("x1");
+	expect(gameLogic.valitadeInput("X1")).toBe("x1");
 });
