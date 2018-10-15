3cdfb265db750e8fa8f2bcf859bd0aaf5e5762d7
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-15 21:35:23 +0000

    Implemented a function to make a move

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -7,15 +7,23 @@ module.exports = {
 		if( !validationMask.exec(input) ){		
 			throw input + " is not a valid input only acceptable input is on the form x1 or o1";
 		}
+		if ( input.includes("0") ){
+			throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+		}
 		return input.toLowerCase();
 	},
-	makeAMove: function(gameState, Move){
-		return true;
+	makeAMove: function(gameState, move){
+
+		if ( gameState[ parseInt(move[1]) - 1 ]  != 'x' && gameState[ parseInt(move[1]) - 1 ] != 'o' ){
+			gameState[ parseInt(move[1]) - 1 ] = move[0];
+			return gameState;
+		}
+		return false;
 	},
-	checkIfMoveIsLeagal: function( field, move ){
+	checkIfMoveIsLeagal: function( field ){
 		if(field == 'x' || field == 'o'){
 			return false;
 		}
 		return true;
-	}
+	},
 };
--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -13,13 +13,34 @@ it("Should throw an error with Error code 'bb is not a valid input only acceptab
 	}
 	expect(tryInvalid).toThrowError("bb is not a valid input only acceptable input is on the form x1 or o1");
 });
+it("Should throw an error with Error code 'bb is not a valid input only acceptable input is on the form x1 or o1'", () => {
+	function tryInvalid (){
+		gameLogic.valitadeInput('0x');
+	}
+	expect(tryInvalid).toThrowError("0x is not a valid input only acceptable input is on the form x1 or o1");
+});
 it("should return x1", () => {
 	expect(gameLogic.valitadeInput("X1")).toBe("x1");
 });
 
+it("should return ['x','2','3','4','5','6','7','8','9']", () => {
+	expect(gameLogic.makeAMove(['1','2','3','4','5','6','7','8','9'], 'x1' )).toEqual(['x','2','3','4','5','6','7','8','9']);
+});
 
-it("should return true", () => {
-	expect(gameLogic.makeAMove(['1','2','3','4','5','6','7','8','9'], 'x1' )).toBe(true);
+it("should return ['o','2','3','4','5','6','7','8','9']", () => {
+	expect(gameLogic.makeAMove(['1','2','3','4','5','6','7','8','9'], 'o1' )).toEqual(['o','2','3','4','5','6','7','8','9']);
+});
+
+it("should return ['1','2','3','4','5','6','7','8','x']", () => {
+	expect(gameLogic.makeAMove(['1','2','3','4','5','6','7','8','9'], 'x9' )).toEqual(['1','2','3','4','5','6','7','8','x']);
+});
+
+it("should return false", () => {
+	expect(gameLogic.makeAMove(['1','2','3','4','5','6','7','8','x'], 'o9' )).toEqual(false);
+});
+
+it("should return false", () => {
+	expect(gameLogic.makeAMove(['1','2','3','4','5','6','7','8','x'], 'x9' )).toEqual(false);
 });
 
 it("should return true", () => {
