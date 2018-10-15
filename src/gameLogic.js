d73800ef48dcd7c9c30846be1547b973c808b539
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-15 14:17:29 +0000

    Added a function that valitades user input

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,6 +1,13 @@
-function readInput(input){
-
-	throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+function valitadeInput(input){
+	if(input.length != 2){
+		throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+	}
+	var validationMask = /([xXoO(\d)])([(\d)xXoO])/;
+	if( !validationMask.exec(input) ){
+		
+		throw input + " is not a valid input only acceptable input is on the form x1 or o1";
+	}
+	return input.toLowerCase();
 }
 
-module.exports = readInput;
+module.exports = valitadeInput;
--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -7,4 +7,12 @@ it("Should throw an error with Error code 'notValidInput is not a valid input on
 	}
 	expect(tryInvalid).toThrowError("notValidInput is not a valid input only acceptable input is on the form x1 or o1");
 });
-
+it("Should throw an error with Error code 'bb is not a valid input only acceptable input is on the form x1 or o1'", () => {
+	function tryInvalid (){
+		gameLogic('bb');
+	}
+	expect(tryInvalid).toThrowError("bb is not a valid input only acceptable input is on the form x1 or o1");
+});
+it("should return x1", () => {
+	expect(gameLogic("X1")).toBe("x1");
+});
