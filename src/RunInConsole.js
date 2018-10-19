79211633bddde7b445040195017d5c7c624900d1
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-19 14:54:51 +0000

    created some sort of methood to read user input

--- src/RunInConsole.js
+++ src/RunInConsole.js
@@ -1,13 +1,22 @@
-//const readline = require('readline').createInterface({
-//  input: process.stdin,
-//  output: process.stdout
-//});
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-module.exports = {
-	Run: function(){
-		return 0;
-	},
-	ReadUserInput: function(){
-//		readline.question('Write in youre move ', (move) => { readline.close(); })
+exports.Run = function(){
+	var isGameOver = false;
+	while(!isGameOver) {
+		var input = ReadUserInput();
+		isGameOver = sendInUserInput(input);
 	}
+};
+
+function ReadUserInput(){
+		readline.question('Write in youre move ', (move) => { readline.close(); })
+		return "move";
+
 }
+function sendInUserInput(){
+		return false;
+}
+
