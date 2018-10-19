fd2defc1e079a9f588b02730d0d5d39e712da047
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-19 13:44:14 +0000

    Created a test for run in console (main reson for this commit is to be able to pull)

--- src/RunInConsole.js
+++ src/RunInConsole.js
@@ -1,5 +1,13 @@
+//const readline = require('readline').createInterface({
+//  input: process.stdin,
+//  output: process.stdout
+//});
+
 module.exports = {
 	Run: function(){
-		console.log("hello!!!");
+		return 0;
+	},
+	ReadUserInput: function(){
+//		readline.question('Write in youre move ', (move) => { readline.close(); })
 	}
 }
