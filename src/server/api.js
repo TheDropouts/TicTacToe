2807b1c0d0974736fe5b53bd233576978a77d6ea
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-24 20:02:24 +0000

    Refactirong api call code prepering it to read from file, and starded to write test for api

--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const game = require("../logic/gameBoard");
+const game = require("../logic/apiHandler");
 router.get("/gamestate", (req, res) => {
   res.status(200).send( game.sendGameStatus() );
 });
@@ -10,4 +10,8 @@ router.get("/makeAMove/:move", (req, res) => {
 router.get("/reset", (req, res) => {
   res.status(200).send( game.resetGameBoard() );
 });
+router.get("/victoryCheck", (req, res) => {
+  res.status(200).send( game.victoryCheck() );
+});
+
 module.exports = router;
--- src/server/api.test.js
+++ src/server/api.test.js
@@ -3,11 +3,20 @@ const app = require("../../app");
 
 describe("GET endpoint with name should return with statuscode", () => {
   it("Should return status code 200", async () => {
-    const res = await request(app).get("/gamestate");
+    const res = await request(app).get("/api/gamestate");
     expect(res.status).toBe(200);  
   });
   it("Should return current gamestate", async () => {
-    const res = await request(app).get("/gamestate");
-    expect(res.status).toBe(200);  
+    const res = await request(app).get("/api/gamestate");
+    expect(res.text).toBe('[1,2,3,4,5,6,7,8,9]');  
   });
+
+  it("Should return updated gamestate", async () => {
+
+	await request(app).get("/api/makeAMove/1");
+    const res = await request(app).get("/api/gamestate");
+    //expect(res.text).toBe('[x,2,3,4,5,6,7,8,9]');  
+  });
+
+	
 });
