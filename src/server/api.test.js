d94db22f1520a8ce71509f121cf5a3b89a121064
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-24 10:01:28 +0000

    fixed the test and added a script to start the server

--- src/server/api.test.js
+++ src/server/api.test.js
@@ -3,7 +3,7 @@ const app = require("../../app");
 
 describe("GET endpoint with name should return with statuscode", () => {
   it("Should return status code 200", async () => {
-    const res = await request(app).get("PlayGame");
+    const res = await request(app).get("/PlayGame");
     expect(res.status).toBe(200);  
   });
 });
