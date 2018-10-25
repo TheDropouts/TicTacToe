48f7ac44b73f40556df845863014c701f84ffc01
Stefan Hrafnsson <stefanorn92@gmail.com>
2018-10-25 09:50:17 +0000

    Added a event on each cell so that it can talk to the api and update it self (and the whole table)

--- src/client/index.js
+++ src/client/index.js
@@ -3,12 +3,21 @@ var $ = require("jquery");
 window.onload=function(){
 	
 	var table = $('td');
+	for(var i = 0; i < table.length; i++){
+		
+		var cellNumber = table[i].id;
+		table[i].addEventListener("click", function(j){
+			$.get( "api///makeAMove/" + $(this).attr('id'), function( data ) {
+				console.log($(this).attr('id'));
+			});
+		});
+	}
 
 // búa til function sem getu hverri cellu takka og hver af þessum tökkum getur kallað á api
 // eina sem hver takki þarf að eiga er upplýsingar um hvaða númer er á honum
 
 	$.get( "api//gamestate", function( data ) {
-		console.log(data);
+
 		ChangeTableVal(data);
 	});
 	
@@ -16,9 +25,6 @@ window.onload=function(){
 		for(var i = 0; i < table.length; i++){
 			table[i].innerText = data[i];
 			
-			//tableRow[i].addEventListener("click", function(j){
-	
-			//});
 		}
 	};
 // búa til function sem looðar o geggnum allar cellurnar og uppfærir hvað á a vera í þeim
